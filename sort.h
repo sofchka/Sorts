bf38a3fae042eba3170f348beeec43cc1d11d842
Sofi Zakaryan <164770022+sofchka@users.noreply.github.com>
2025-02-23 15:01:51 +0400

    Update sort.h

--- sort.h
+++ sort.h
@@ -3,6 +3,8 @@
 
 void swap(int *a, int *b);
 void bubble_sort(int a[], int len);
+void bubble_sort_opt(int a[], int len);
+void bubble_sort_recurs(int a[], int len);
 void insertion_sort(int a[], int len);
 void heap_sort(int a[], int len);
 void quick_sort(int a[], int low, int high);
