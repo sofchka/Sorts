fc13545d050e1732d627558feb282c3e3af11618
Sofi Zakaryan <164770022+sofchka@users.noreply.github.com>
2025-02-23 15:00:41 +0400

    Update sorts.c

--- sorts.c
+++ sorts.c
@@ -18,6 +18,45 @@ void	bubble_sort(int a[], int len)
 	}
 }
 
+// Bubble Sort Optimized
+void	bubble_sort_opt(int a[], int len)
+{
+	for (int i = 0; i < len - 1; ++i)
+	{
+		int swapped = 0;
+		for (int j = 0; j < len - i - 1; ++j)
+		{
+			if (a[j] > a[j + 1])
+			{
+				swap (&a[j], &a[j + 1]);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
+
+// Bubble Sort Recursive
+void	bubble_sort_recurs(int a[], int len)
+{
+	if (len == 1)
+		return ;
+
+	int swapped = 0;
+	for (int i = 0; i < len - 1; ++i)
+	{
+		if (a[i] > a[i + 1])
+		{
+			swap(&a[i], &a[i + 1]);
+			swapped = 1;
+		}
+	}
+	if (swapped == 0)
+		return ;
+	bubble_sort_recurs(a, len - 1);
+}
+
 // Insertion Sort
 void	insertion_sort(int a[], int len)
 {
